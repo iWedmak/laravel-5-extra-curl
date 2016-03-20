ec3fe579f2cf4beb3d4dd48c9008b340a70bc393
wedmak <wedmak@msk1.ethdns.com>
2016-03-20 11:37:35 +0300

    more complex file fet contents, so we have proxy oprion on it (testing in order) and we have user agent set method

--- src/Parser.php
+++ src/Parser.php
@@ -6,6 +6,9 @@ use MarkWilson\XmlToJson\XmlToJsonConverter;
 class Parser {
 
     public $c;
+    public $header;
+    public $proxy;
+    public $opts;
     
     public function __construct()
     {
@@ -15,6 +18,21 @@ class Parser {
         //$this->c->setopt(CURLOPT_RETURNTRANSFER, true);
         $this->c->setCookie('language', 'en_EN');
         $this->c->setCookie('lw', 's');
+        $this->header['User-Agent']='Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10';
+        $this->header['Accept-language']='en';
+        $this->header['Cookie']='language=en_EN; lw=s';
+        $this->proxy=false;
+        $this->opts= array(
+            'http'=>array(
+                'method'=>"GET",
+                'request_fulluri'=>true,
+            )
+        );
+        /*
+        "Accept-language: en\r\n" .
+        "Cookie: language=en_EN; lw=s\r\n".
+        "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n"
+        //*/
         //$this->c->setopt(CURLOPT_SSL_VERIFYPEER, false);
         //$this->c->setopt(CURLOPT_SSL_VERIFYHOST, false);
         //$this->c->setopt(CURLOPT_CAPATH, "sddsdf/cacert.pem");
@@ -38,26 +56,24 @@ class Parser {
         }
         else
         {
-            switch ($type) {
+            switch ($type) 
+            {
                 case 'curl':
                     $this->c->get($url);
                     $responce=$this->c->response;
                     break;
                 case 'file':
-                    $opts = array(
-                      'http'=>array(
-                        'method'=>"GET",
-                        'header'=>"Accept-language: en\r\n" .
-                                  "Cookie: language=en_EN; lw=s\r\n".
-                                  "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n"
-                        )
-                    );
-                    $context = stream_context_create($opts);
-                    pre($url);
+                    $header = implode("\r\n", array_map(
+                        function ($v, $k) { return $k . ':' . $v; }, 
+                        $this->header, 
+                        array_keys($this->header)
+                    ));
+                    //pre($header);
+                    $this->opts['http']['header']=$header;
+                    $context = stream_context_create($this->opts);
                     $responce = @file_get_contents($url, false, $context);
                     break;
             }
-            
             $doc = @simplexml_load_string($responce, 'SimpleXMLElement', LIBXML_NOCDATA);
             if ($doc) {
                 $converter = new XmlToJsonConverter();
@@ -76,7 +92,9 @@ class Parser {
     {
         if($type=='mobile')
         {
-            $this->c->setopt(CURLOPT_USERAGENT, 'Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30');
+            $str='Mozilla/5.0 (Linux; U; Android 4.0.3; de-ch; HTC Sensation Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30';
+            $this->c->setopt(CURLOPT_USERAGENT, $str);
+            $this->header['User-Agent']=$str;
         }
         else
         {
@@ -86,6 +104,7 @@ class Parser {
         if($agent)
         {
             $this->c->setopt(CURLOPT_USERAGENT, $agent);
+            $this->header['User-Agent']=$agent;
         }
     }
     
@@ -95,7 +114,8 @@ class Parser {
         $resp=$this->get($url, 30, 'file');
         $array=explode(PHP_EOL, $resp);
         $proxy=$array[rand(0,count($array)-1)];
-        $this->c->setopt(CURLOPT_PROXY, $proxy);
+        $this->opts['http']['proxy']=$this->proxy;
+        $this->c->setopt(CURLOPT_PROXY, $this->proxy);
     }
     
     public function returnHeaders()
